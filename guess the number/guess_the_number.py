7e974ebd3bef05de94fa24b30be15f383456cca8
ajay <musk.paradox.0@gmail.com>
2021-03-15 23:57:40 +0530

    guess the no. commit

---
+++ guess the number/guess_the_number.py
@@ -0,0 +1,23 @@
+n = 30
+chance = 0
+print("you have 7 chance to guess the number.")
+
+while chance < 7 :
+
+    guess = int(input("enter a number: "))
+    chance = chance + 1
+
+    if guess > 30 :
+        print("number is less than your number.")
+        print("you have", 7 - chance, "chance left.")
+    elif guess < 30 :
+        print("number is greater than your number.")
+        print("you have", 7 - chance, "chance left.")
+    elif guess == 30:
+        print("correct ! yeahh this is the number. ")
+        print("you take ",chance,"chance to guess the correct number.")
+        break
+    elif chance >= 7:
+        print("game over")
+
+        break
